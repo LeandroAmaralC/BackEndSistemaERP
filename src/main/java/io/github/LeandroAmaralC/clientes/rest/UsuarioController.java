e6dd0e13ed08fd82ceddd939b4263d111521e8f8
Leandro Correia <lcorreia35@gmail.com>
2022-08-25 21:49:33 -0300

    salvarExcecao

--- src/main/java/io/github/LeandroAmaralC/clientes/rest/UsuarioController.java
+++ src/main/java/io/github/LeandroAmaralC/clientes/rest/UsuarioController.java
@@ -1,11 +1,14 @@
 package io.github.LeandroAmaralC.clientes.rest;
 
 
+import io.github.LeandroAmaralC.clientes.exception.UsuarioCadastradoException;
 import io.github.LeandroAmaralC.clientes.model.enity.Usuario;
 import io.github.LeandroAmaralC.clientes.model.repository.UsuarioRepository;
+import io.github.LeandroAmaralC.clientes.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 
@@ -14,11 +17,17 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class UsuarioController {
 
-    private final UsuarioRepository repository;
+    private UsuarioService service;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void salvar (@RequestBody @Valid Usuario usuario ) {
-        repository.save(usuario);
+    public void salvar (@RequestBody @Valid Usuario usuario ){
+        try{
+            service.salvar(usuario);
+        }catch (UsuarioCadastradoException e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+
+
     }
 }
